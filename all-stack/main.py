7a71bfaa30227ca4637509af0a167daf05e099d5
navindu-ds <navindu.20@cse.mrt.ac.lk>
2023-12-31 16:57:42 +0530

    Added Test Cases for testing purposes

--- all-stack/main.py
+++ all-stack/main.py
@@ -1,5 +1,7 @@
 import all_stack_start
 
+is_max = True
+
 # matrix = [[-5, -4, 0, 0, 0], [6, 4, 1, 0, 24], [1, 2, 0, 1, 6]]
 # basic_vars = [0, 3, 4]
 
@@ -9,12 +11,40 @@ import all_stack_start
 # matrix = [[-3, 1, 0, 0, 0], [4, -1, 1, 0, 8], [-8, -1, 0, 1, -12]]
 # basic_vars = [0, 3, 4]
 
-# # failed case
+# matrix = [[-19,-13,-12,-17,  0,  0,  0,  0],
+#           [  3,  2,  1,  2,  1,  0,  0,225],
+#           [  1,  1,  1,  1,  0,  1,  0,117],
+#           [  4,  3,  3,  4,  0,  0,  1,420]]
+# basic_vars = [0, 5, 6, 7]
+
+# fail - divide by zero error
+# matrix = [[  1, -2,  1,  0,  0,  0,  0],
+#           [  1,  2,  1,  1,  0,  0, 12],
+#           [  2,  1, -1,  0,  1,  0,  6],
+#           [ -1,  3,  0,  0,  0,  1,  9]]
+# basic_vars = [0, 4, 5, 6]
+# is_max = False
+
+matrix = [[ -5, -6,  0,  0,  0],
+          [ -1, -1,  1,  0, -2],
+          [ -4, -2,  0,  1, -4]]
+basic_vars = [0, 3, 4]
+is_max = False
+
+# # failed case - infinite loop
+# matrix = [[-3,  1, -1,  0,  0,  0,  0], 
+#           [ 4, -1,  0,  1,  0,  0,  8],
+#           [-8, -1, -3,  0,  1,  0,-12],
+#           [ 5,  0, -1,  0,  0,  1, 13]]
+# basic_vars = [0, 4, 5, 6]
+
+# # failed case - infinite loop
 # matrix = [[1, -2, 0, 0, 0, 0], [-1, -1, 1, 0, 0, -2], [1, -1, 0, 1, 0, -1], [5, 0, 0, 0, 1, 3]]
 # basic_vars = [0, 3, 4, 5]
 
-matrix = [[-3, 1, -1, 0, 0, 0, 0], [4, -1, 0, 1, 0, 0, 8], [-8, -1, -3, 0, 1, 0, -12], [5, 0, -1, 0, 0, 1, 13]]
-basic_vars = [0, 4, 5, 6]
+# # failed case - infinite loop
+# matrix = [[-3, 1, -1, 0, 0, 0, 0], [4, -1, 0, 1, 0, 0, 8], [-8, -1, -3, 0, 1, 0, -12], [5, 0, -1, 0, 0, 1, 13]]
+# basic_vars = [0, 4, 5, 6]
 
 # print(next_simplex_sol(basic_vars, matrix, True))
-all_stack_start.solve_linear_programming(basic_vars, matrix, True)
+all_stack_start.solve_linear_programming(basic_vars, matrix, is_max)
