d3753be2b7a0513e991646c4da0a7a84c7da03bb
navindu-ds <navindu.20@cse.mrt.ac.lk>
2024-04-27 07:52:49 +0530

    add init files to simplify imports

---
+++ elpee/algorithms/__init__.py
@@ -0,0 +1,15 @@
+# elpee/algorithms/__init__.py
+
+from .AllStackStarter import AllStackStarter
+from .AlternateSolver import AlternateSolver
+"""
+Export Algorithms to solve Linear Programming Problems
+
+Modeules exported by this package:
+
+- `AllStackStarter` : Provide implementation for All Atack Starting Method
+- `AlternateSolver` : Provide implementation for developing alternate solutions
+- `bigM_handler` : Provide implementation for Big M method
+- `DualSimplexSolver` : Provide implementation for Dual Simplex method
+
+"""
